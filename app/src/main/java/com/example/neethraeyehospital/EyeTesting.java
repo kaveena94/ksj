7f02af7b34218c44d99748b3368ebc9ccabd1835
kaveena94 <kaveenasj@gmail.com>
2019-08-06 11:35:10 +0530

    Done

---
+++ app/src/main/java/com/example/neethraeyehospital/EyeTesting.java
@@ -0,0 +1,14 @@
+package com.example.neethraeyehospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EyeTesting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eye_testing);
+    }
+}
